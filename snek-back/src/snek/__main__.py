5fc0bce4d233905f8ca968cddd28216bc7080e90
Jorre <housenjorre@gmail.com>
2023-03-21 22:07:13 +0100

    Added fastapi

--- snek-back/src/snek/__main__.py
+++ snek-back/src/snek/__main__.py
@@ -2,15 +2,23 @@ import os
 
 import click
 import logger_provider
+import uvicorn
+from core.config import config
 
 
 @click.command()
 @click.option(
     "--env",
-    type=click.Choice(["dev", "prd"], case_sensitive=False),
-    default="dev",
+    type=click.Choice(["local", "dev", "prod"], case_sensitive=False),
+    default="local",
 )
-def main(env: str):
+@click.option(
+    "--debug",
+    type=click.BOOL,
+    is_flag=True,
+    default=False,
+)
+def main(env: str, debug: bool):
     """Main function, start coding here"""
     logger_provider.set_logger_level(env)
 
@@ -19,6 +27,14 @@ def main(env: str):
     log.info("This is a information log")
     log.error("This is a Error log")
     log.critical("This is a Critical log")
+
+    uvicorn.run(
+        app="app.server:app",
+        host=config.APP_HOST,
+        port=config.APP_PORT,
+        reload=True if config.ENV != "production" else False,
+        workers=1,
+    )
 
 
 if __name__ == "__main__":
